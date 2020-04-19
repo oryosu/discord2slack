db71e7c95e95100f3349afa25dc19cf581ff240f
Ryosuke Sonobe <ryosuke.sonobe@applebum.local>
2020-04-19 23:33:07 +0900

    fix bug

--- discord2slack.rb
+++ discord2slack.rb
@@ -94,7 +94,7 @@ bot.servers.each_value do |srv|
             res = gyazo.upload imagefile: "notification/#{channel}.jpg"
             pp res[:permalink_url]
             gyazo.delete image_id: res[:image_id]
-            #client.chat_postMessage(channel: '#discord_observer_develop', text: "Now #{users.join(', ')} in ##{channel}\n#{res[:permalink_url]}")
+            client.chat_postMessage(channel: '#discord_observer_develop', text: "Now #{users.join(', ')} in ##{channel}\n#{res[:permalink_url]}")
         end
     end
 end
