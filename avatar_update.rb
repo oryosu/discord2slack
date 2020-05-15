042fbb255ea71df5866358c873ff2a93f9e013dd
oryosu <knocksdownoryosu@gmail.com>
2020-05-15 14:25:27 +0900

    use s3

--- avatar_update.rb
+++ avatar_update.rb
@@ -20,11 +20,12 @@ bot.run(async=true)
 bot.servers.each_value do |srv|
     srv.users.each do |user|
         pp user.avatar_url
-        File.open("tmp/#{user.name}.jpg", "wb") do |file|
-            open(user.avatar_url) do |img|
-                file.puts img.read
-            end
+        obj = s3.bucket('discord2slack-for-dp9').object("orig/#{user.name}.jpg")
+        open(user.avatar_url) do |img|
+            obj.puts(body: img)
         end
+        obj = s3.bucket('discord2slack-for-dp9').object("orig/#{user.name}.jpg")
+        obj.get(response_target: "tmp/#{user.name}.jpg")
         img = Magick::ImageList.new("tmp/#{user.name}.jpg")
         # 新しいサイズへ変更
         img = img.resize_to_fit(128,128)
