8fb78c677206ad12fd05e272af4f8b8a321529f0
a1r3us <38165459+a1r3us@users.noreply.github.com>
2021-11-29 22:15:42 +0900

    skip image gen for new channels

--- discord2slack.rb
+++ discord2slack.rb
@@ -72,7 +72,7 @@ bot.servers.each_value do |srv|
         # 新画像保存
         #img.write("avatar/#{user.name}.jpg")
     #    end
-    
+
 #サーバからボイスチャンネルにいるユーザーを取得
     active_users = []
     pp srv.voice_states
@@ -89,33 +89,37 @@ bot.servers.each_value do |srv|
     pp voice_status
     voice_status.each do |channel, users|
         if !(users == []) then
-            imagelist = []
-            users.each do |user|
-                avatar = bucket.object("avatar/#{user}.jpg")
-                avatar.download_file("avatar/#{user}.jpg")
-                imagelist.push(avatar.key)
+            if channel_with_bg.include?(channel) then
+                imagelist = []
+                users.each do |user|
+                    avatar = bucket.object("avatar/#{user}.jpg")
+                    avatar.download_file("avatar/#{user}.jpg")
+                    imagelist.push(avatar.key)
+                end
+                avatars = Magick::ImageList.new(*imagelist)
+                avatars = avatars.append(false)
+                bg = bucket.object("bg/#{channel}.jpg")
+                bg.download_file("bg/#{channel}.jpg")
+                pp bg
+                bg = Magick::Image.read(bg.key).first
+                bg.composite!(avatars, Magick::SouthWestGravity, Magick::OverCompositeOp)
+                t = Time.new
+                timestamp = t.strftime("%Y%m%d%H%M%S")
+                draw = Magick::Draw.new
+                draw.font = 'Verdana-Bold'
+                draw.pointsize = 5
+                draw.gravity = Magick::CenterGravity
+                draw.annotate(bg, 16, 16, 1250, 0, timestamp)
+                pp timestamp
+                bg.write("#{channel}.jpg")
+                #notification_img = bucket.object("notification/#{channel}.jpg")
+                res = gyazo.upload imagefile: "#{channel}.jpg"
+                pp res[:permalink_url]
+                client.chat_postMessage(channel: '#000_discord', text: "Now #{users.join(', ')} in ##{channel}\n#{res[:permalink_url]}")
+                #client.chat_postMessage(channel: '#discord_observer_develop', text: "Now #{users.join(', ')} in ##{channel}\n#{res[:permalink_url]}")
+            else
+                client.chat_postMessage(channel: '#000_discord', text: "Now #{users.join(', ')} in ##{channel}")
             end
-            avatars = Magick::ImageList.new(*imagelist)
-            avatars = avatars.append(false)
-            bg = bucket.object("bg/#{channel}.jpg")
-            bg.download_file("bg/#{channel}.jpg")
-            pp bg
-            bg = Magick::Image.read(bg.key).first
-            bg.composite!(avatars, Magick::SouthWestGravity, Magick::OverCompositeOp)
-            t = Time.new
-            timestamp = t.strftime("%Y%m%d%H%M%S")
-            draw = Magick::Draw.new
-            draw.font = 'Verdana-Bold'
-            draw.pointsize = 5
-            draw.gravity = Magick::CenterGravity
-            draw.annotate(bg, 16, 16, 1250, 0, timestamp)
-            pp timestamp
-            bg.write("#{channel}.jpg")
-            #notification_img = bucket.object("notification/#{channel}.jpg")
-            res = gyazo.upload imagefile: "#{channel}.jpg"
-            pp res[:permalink_url]
-            client.chat_postMessage(channel: '#000_discord', text: "Now #{users.join(', ')} in ##{channel}\n#{res[:permalink_url]}")
-            #client.chat_postMessage(channel: '#discord_observer_develop', text: "Now #{users.join(', ')} in ##{channel}\n#{res[:permalink_url]}")
         end
     end
 end
